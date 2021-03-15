9c822c1ab6221496a6f18f45c39963624e95bdf4
bobwilson5 <bob.wilson.github@gmail.com>
2021-03-15 09:16:14 +0530

    Remote update 1

--- src/main/java/com/gokoders/ourfirstproject/OurfirstprojectApplication.java
+++ src/main/java/com/gokoders/ourfirstproject/OurfirstprojectApplication.java
@@ -8,6 +8,7 @@ public class OurfirstprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OurfirstprojectApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
