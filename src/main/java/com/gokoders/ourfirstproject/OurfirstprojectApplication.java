6782365d035aa2b34bc7d81a04b2a43292e5f955
Bibhash Roy <bibhash.roy@gmail.com>
2021-03-15 07:41:23 +0530

    initial commit for Ourfirstproject application

---
+++ src/main/java/com/gokoders/ourfirstproject/OurfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gokoders.ourfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurfirstprojectApplication.class, args);
+	}
+
+}
